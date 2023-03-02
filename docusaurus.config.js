9d987ddf0a2c940b3cf7231a49c0f2c1550e8a96
Kane Petra <kane.petra@looplang.org>
2023-03-02 23:04:26 +0100

    tweak: ignore broken links i18n

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'mindspaces-fontys', // Usually your GitHub org/user name.
   projectName: 'mindspaces.github.io', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
